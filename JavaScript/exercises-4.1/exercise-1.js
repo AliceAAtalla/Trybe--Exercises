b8a66f1cbfba457b3e616a03c7a531a167bb07ae
AliceAAtalla <aliceatalla36@gmail.com>
2020-03-26 17:46:01 -0300

    Pasta exercises-4.1 - add exercise-1.js

---
+++ JavaScript/exercises-4.1/exercise-1.js
@@ -0,0 +1,26 @@
+/*Faça cinco programas, um para cada operação aritmética básica. 
+Seu programa deve ter duas variáveis, a e b, definidas no começo 
+com os valores que serão operados. Faça programas para:*/
+
+let a = 2;
+let b = 3;
+
+let adicao = a + b;
+
+console.log(adicao)
+
+let subtracao = a - b;
+
+console.log(subtracao)
+
+let multiplicacao = a * b;
+
+console.log(multiplicacao)
+
+let divisao = a / b;
+
+console.log(divisao)
+
+let modulo = a % b;
+
+console.log(modulo)
