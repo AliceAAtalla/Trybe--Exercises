5f672417cc3d1c1f7898a94927188b4104cc569d
AliceAAtalla <aliceatalla36@gmail.com>
2020-03-26 17:54:25 -0300

    exercise-2.js

---
+++ JavaScript/exercises-4.1/exercise-2.js
@@ -0,0 +1,15 @@
+/* Faça um programa que retorne o maior de dois números.
+ Defina no começo do programa duas variáveis com os valores
+ que serão comparados.*/
+
+let val1 = 6;
+let val2 = 3;
+
+if (val1 > val2) {
+    result = val1;
+}
+else {
+    result = val2;
+}
+
+console.log(result);
