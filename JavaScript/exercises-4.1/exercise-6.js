1da596c1a6073fa9eef14bfb333f7202b47f9497
AliceAAtalla <aliceatalla36@gmail.com>
2020-03-30 07:53:13 -0300

    Update exercises 5-6

--- JavaScript/exercises-4.1/exercise-6.js
+++ JavaScript/exercises-4.1/exercise-6.js
@@ -8,26 +8,28 @@ Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
 
 Exemplo: bishop -> diagonals*/
 
-let piece = "Queen";
+let piece = "Qeen";
 piece = piece.toLowerCase();
 
 switch (piece) {
-  case "king":
-    console.log("The king moves exactly one vacant square in any direction: forwards, backwards, left, right, or diagonally;");
-  break;
-  case "queen":
-    console.log("The queen moves any number of vacant squares in any direction: forwards, backwards, left, right, or diagonally, in a straight line.");
-  break;
-  case "rooks":
-    console.log("The rook moves any number of vacant squares forwards, backwards, left, or right in a straight line.");
-  break;
-  case "bishops":
-    console.log("The bishop moves any number of vacant squares diagonally in a straight line.");
-  break;
-  case "knights":
-    console.log("The knight moves on an extended diagonal from one corner of any 2×3 rectangle of squares to the furthest opposite corner. ");
-  break;
-  case "pawns":
-    console.log("The pawn moves forward exactly one square, or optionally, two squares when on its starting square, toward the opponent's side of the board.");
-  break;
+    case "king":
+      console.log("The king moves exactly one vacant square in any direction: forwards, backwards, left, right, or diagonally;");
+    break;
+    case "queen":
+      console.log("The queen moves any number of vacant squares in any direction: forwards, backwards, left, right, or diagonally, in a straight line.");
+    break;
+    case "rooks":
+      console.log("The rook moves any number of vacant squares forwards, backwards, left, or right in a straight line.");
+    break;
+    case "bishops":
+      console.log("The bishop moves any number of vacant squares diagonally in a straight line.");
+    break;
+    case "knights":
+      console.log("The knight moves on an extended diagonal from one corner of any 2×3 rectangle of squares to the furthest opposite corner. ");
+    break;
+    case "pawns":
+      console.log("The pawn moves forward exactly one square, or optionally, two squares when on its starting square, toward the opponent's side of the board.");
+    break;
+    default:
+      console.log("Error")
   }
