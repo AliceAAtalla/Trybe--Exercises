630717051611870643e7bbfe6eed39615f50f812
AliceAAtalla <aliceatalla36@gmail.com>
2020-03-30 08:23:14 -0300

    Add exercises_4.2

---
+++ JavaScript/exercises-4.2/exercise-2.js
@@ -0,0 +1,10 @@
+/* Para o segundo exercício, faça o mesmo que antes, 
+mas que imprima um triângulo retângulo com 5 asteriscos de base */
+
+let n = 5;
+let padrao = "";
+
+for (let linha = 1; linha <= n; linha +=1) {
+    padrao += "*";
+    console.log(padrao)
+}
