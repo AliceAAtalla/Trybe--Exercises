8b6470f02941ffe3f4506078533e9fe815ac84e2
AliceAAtalla <aliceatalla36@gmail.com>
2020-03-29 23:18:36 -0300

    Add exercise-8.js

---
+++ JavaScript/exercises-4.1/exercise-8.js
@@ -0,0 +1,14 @@
+/*Escreva um programa que defina três números em 
+variáveis no seu começo e retorne true se uma das 
+três for par. Caso contrário, ele retorna false.
+
+Bonus: use somente um if.*/
+
+let num = (3, 5, 4);
+let isEven = true;
+
+if((num / 2) % 1){
+  isEven = false;
+}
+
+console.log(isEven);
